5f924a97e3d827c8161fb5707435edd34dacb060
Fiza Ali <92218448+darakhshad11@users.noreply.github.com>
2023-09-11 13:24:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,17 @@ def get_image_text(filename):
 image_filename ="image.jpg"
 
 print(get_image_text(image_filename))
+
+
+
+import pytesseract
+from PIL import Image
+
+def get_image_text(filepath):
+    myconfig = "--psm 6 --oem 3"
+    text = pytesseract.image_to_string(Image.open(filepath), config=myconfig)
+    return text
+
+image_filepath = r"C:\ekbet\Screenshot (107).png"
+
+print(get_image_text(image_filepath)
